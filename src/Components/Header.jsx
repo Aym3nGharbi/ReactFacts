8d1eb4e4536445e59070996e6f36e7fae4ccf6b4
Aymen <agharbi326@gmail.com>
2025-06-20 12:29:35 +0100

    intial commit ready for deploy

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from '../assets/react.svg'
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} />
+      <h2>ReactFacts</h2>
+    </header>
+  )
+}
---
+++ src/Components/MainContent.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <main>
+      <h1>Fun Facts About React</h1>
+      <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 200k starts on Github</li>
+          <li>Is maintened by Meta</li>
+          <li>Powers thousands of enterprise apps, including Mobile apps</li>
+      </ul>
+    </main>
+  )
+}
